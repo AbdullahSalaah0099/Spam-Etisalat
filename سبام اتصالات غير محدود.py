d46a0588a67d02fb3cc216380499f1e9bf3c4294
Abdullah Salah <95449686+AbdullahSalaah0099@users.noreply.github.com>
2022-06-25 22:14:36 +0200

    Update سبام اتصالات غير محدود.py

--- سبام اتصالات غير محدود.py
+++ سبام اتصالات غير محدود.py
@@ -8,17 +8,17 @@ print ()
 print ('\033[1;92mclick on this link to get password👇')
 #sleep (0.1)
 print ()
-link="\033[1;93m https://miklpro.com/IRZv9a"
+link="\033[1;93m https://miklpro.com/9aw3qf"
 print (link)
 print()
 password=input ('\033[1;92m》Enter Password Script :  \033[1;96m')
 sleep (1)
 
-rrr=requests.get('https://pastelink.net/v43csjnv').text
+'''rrr=requests.get('https://pastelink.net/v43csjnv').text
 soup=BS(rrr,'html.parser')
-lxc=(soup.find('div',{'class':'body-display'})).text
+lxc=(soup.find('div',{'class':'body-display'})).text'''
 
-if password in lxc:
+if password =="Abdullah3456":
     print ()
     print ('\033[1;96m》True Password《')
     sleep (1)
